a2fc8f7e22410ac17659e83c5b9c2cd02a3ca714
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-01 19:31:11 -0300

    Adding break word

--- src/components/Item/StylesTaskItem.jsx
+++ src/components/Item/StylesTaskItem.jsx
@@ -3,8 +3,12 @@ import styled from "styled-components";
 export const StyledLi = styled.li`
   background-color: #dadada;
   padding: 0.5rem 3rem;
+  min-width: 10rem;
   border-radius: 4px;
   display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  word-break: break-word;
   gap: 1rem;
 `;
 
